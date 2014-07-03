5c5ba35c52486b2323bd9db905561f70c27ffdc4
martin <martin.reisenhofer@funkring.net>
2014-07-04 00:14:55 +0200

    [RM] client-scripts

--- client-scripts/test.py
+++
@@ -1,9 +0,0 @@
-
-if __name__ == '__main__':
-    dict = {1 : "2014-06-12",
-            2 : "2014-01-01",
-            3 : "2015-02-27" }
-    lowest_date = min(dict.values())
-    for key, value in dict.iteritems():
-        if value == lowest_date:
-            print key
