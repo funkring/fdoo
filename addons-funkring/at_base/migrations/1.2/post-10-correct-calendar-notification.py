d24de057cffda38d2122ec5ca41399b1665bc333
martin <martin.reisenhofer@funkring.net>
2014-07-18 10:47:38 +0200

    [FIX] migrations no done extern

--- addons-funkring/at_base/migrations/1.2/post-10-correct-calendar-notification.py
+++ addons-funkring/at_base/migrations/1.2/post-10-correct-calendar-notification.py
@@ -2,4 +2,6 @@
 __name__ = "Correct Calendar Notification"
 
 def migrate(cr,v):
-    cr.execute("DELETE FROM ir_cron WHERE function like '%do_run_scheduler%'")
+    # Not sure if this is good
+    #cr.execute("DELETE FROM ir_cron WHERE function like '%do_run_scheduler%'")
+    pass
