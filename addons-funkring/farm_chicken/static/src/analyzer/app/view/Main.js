26d6dac8cdedcebc86ce4c00cb6838983a8c1a1a
Martin Reisenhofer <martin.reisenhofer@funkring.net>
2015-01-08 11:14:54 +0100

    [TRY]

--- addons-funkring/farm_chicken/static/src/analyzer/app/view/Main.js
+++ addons-funkring/farm_chicken/static/src/analyzer/app/view/Main.js
@@ -1,3 +1,65 @@
+var filterList = Ext.create('Ext.List', {
+   flex : 1,
+   store: {
+     fields: ['name'],
+     data: [{"name":"Test1"}]
+   },
+   itemTpl: '{name}'
+});
+
+var optionPanel = Ext.create('Ext.Panel', {
+   flex : 1,
+   layout : {
+       type : 'vbox'
+   },
+   items : [
+        {
+            xtype : 'toolbar',
+            docked : 'top',
+            items : [
+                {
+                    xtype : 'button',
+                    text
+                    
+                }
+            ]   
+        }
+   ]
+});
+
+var navPanel = Ext.create('Ext.Panel', {    
+    flex : 1,
+    layout: {
+        type : 'vbox'
+    },
+    items : [
+        {
+            xtype: 'toolbar',
+            docked: 'top',
+            items : [
+                {
+                    xtype: 'button',
+                    text: 'Hinzufügen',
+                    handler: function() {
+                        /*
+                        view.push({
+                                         
+                             xtype: 'panel',
+                             flex: 1,
+                             items : [
+                                xtype: 'toolbar',
+                                docked: 'top'
+                             ]                                          
+                        });*/
+                    }
+                }
+            ]
+            
+        },       
+        filterList
+    ]
+});
+
 Ext.define('ChickenFarmAnalyzer.view.Main', {
     extend: 'Ext.Container',
     xtype: 'main',
@@ -9,22 +71,7 @@ Ext.define('ChickenFarmAnalyzer.view.Main', {
             type: 'hbox'
         },        
         items: [            
-            {
-                xtype: 'panel',                
-                flex: 1,                
-                items : [
-                    {
-                        xtype: 'toolbar',
-                        docked: 'top',
-                        items: [
-                            {
-                                xtype: 'button',
-                                text: "Add"
-                            }
-                        ]       
-                    }
-                ]
-            },
+            navPanel,
             {
                 xtype: 'panel',
                 html: 'Chart',
