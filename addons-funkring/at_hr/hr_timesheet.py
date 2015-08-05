f0d8512ff72c3119dcb46d8de3743b5cf41c163b
Edwin Pichler <edwin.pichler@funkring.net>
2015-08-05 18:41:26 +0200

    [FIX][MIG] migrated employee report to odoo8

--- addons-funkring/at_hr/hr_timesheet.py
+++ addons-funkring/at_hr/hr_timesheet.py
@@ -26,8 +26,8 @@ from openerp.addons.at_base import util
 from openerp.addons.at_base import helper
 from dateutil.relativedelta import relativedelta
 
-class hr_timesheet(osv.osv):        
-    
+class hr_timesheet(osv.osv):
+
     def add_totals_day(self, cr, uid, oid, days, context=None):
         res = {}
         cr.execute('SELECT day.name, day.total_attendance, day.total_timesheet, day.total_difference\
@@ -42,11 +42,11 @@ class hr_timesheet(osv.osv):
             res[record[0]]['total_timesheet_day'] = record[2]
             res[record[0]]['total_difference_day'] = record[3]
         return res
-    
-    
-    def get_leaves(self,cr,uid,oid,context=None):                  
+
+
+    def get_leaves(self,cr,uid,oid,context=None):
         return {}
-    
+
     def _get_date_from(self,cr, uid, str_date, context=None):
         d_date = util.strToDate(str_date)
         user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
@@ -70,46 +70,46 @@ class hr_timesheet(osv.osv):
         elif r=='year':
             return d_date.strftime('%Y-12-31')
         return d_date.strftime('%Y-%m-%d')
-    
+
     def get_timesheet_day(self,cr,uid,str_date,context=None):
         """
             return ts_day_obj
         """
         if context is None:
-            context = {}        
+            context = {}
         employee_ids = self.pool.get('hr.employee').search(cr, uid, [('user_id','=',uid)], context=context)
         if employee_ids:
-            employee_id = employee_ids[0]        
+            employee_id = employee_ids[0]
             timesheet_ids = self.search(cr, uid, [('employee_id','=',employee_id),('state','in',['draft','new']),('date_from','<=',str_date), ('date_to','>=',str_date)], context=context)
             if timesheet_ids:
                 ts_day_obj = self.pool.get("hr_timesheet_sheet.sheet.day")
                 ts_day_ids = ts_day_obj.search(cr,uid,[("name","=",str_date),("sheet_id","=",timesheet_ids[0])])
                 if ts_day_ids:
-                    return ts_day_obj.browse(cr,uid,ts_day_ids[0],context)                
-        return None       
-        
+                    return ts_day_obj.browse(cr,uid,ts_day_ids[0],context)
+        return None
+
     def get_timesheet(self,cr,uid,str_date,context=None):
         if context is None:
-            context = {}        
+            context = {}
         employee_ids = self.pool.get('hr.employee').search(cr, uid, [('user_id','=',uid)], context=context)
         if employee_ids:
-            employee_id = employee_ids[0]        
+            employee_id = employee_ids[0]
             timesheet_ids = self.search(cr, uid, [('employee_id','=',employee_id),('date_from','<=',str_date), ('date_to','>=',str_date)], context=context)
             if timesheet_ids:
                 return timesheet_ids[0]
-        return None            
-        
+        return None
+
     def get_timesheet_lazy(self,cr,uid,str_date,context=None):
         if context is None:
             context = {}
-        
+
         employee_ids = self.pool.get('hr.employee').search(cr, uid, [('user_id','=',uid)], context=context)
         if not len(employee_ids):
             raise osv.except_osv(_('Error !'), _('No employee defined for your user !'))
         elif len(employee_ids) > 1:
             raise osv.except_osv(_('Error !'), _('More than one employee defined for your user !'))
-        
-        employee_id = employee_ids[0]        
+
+        employee_id = employee_ids[0]
         timesheet_ids = self.search(cr, uid, [('employee_id','=',employee_id),('state','in',['draft','new']),('date_from','<=',str_date), ('date_to','>=',str_date)], context=context)
         if len(timesheet_ids) > 1:
             raise osv.except_osv(_('Error !'), _('More than one timesheet for the same user and the same time range !'))
@@ -121,48 +121,48 @@ class hr_timesheet(osv.osv):
                 "name" : helper.getMonthYear(cr, uid, str_date, context),
                 "date_from" : date_from,
                 "date_to" : self._get_date_to(cr,uid,str_date,context),
-                "date_current" : date_from,      
+                "date_current" : date_from,
                 "state" : "new",
                 "employee_id" : employee_id,
-                "company_id" : self.pool.get('res.company')._company_default_get(cr, uid, 'hr_timesheet_sheet.sheet', context=context)            
+                "company_id" : self.pool.get('res.company')._company_default_get(cr, uid, 'hr_timesheet_sheet.sheet', context=context)
             }
             return self.create(cr, uid, vals, context=context)
-      
-        
-    def get_timesheet_data(self, cr, uid, oid, context=None):        
+
+
+    def get_timesheet_data(self, cr, uid, oid, context=None):
         #inner function for get lines
         def _get_attendances(in_days, in_day):
             data = in_days.get(in_day,None)
             if data == None:
                 data = {}
                 in_days[in_day] = data
-                
+
             attendance = data.get("attendances",None)
             if attendance == None:
                 attendance = []
                 in_days[in_day]["attendances"]=attendance
-                                
+
             return attendance
-                
-        timesheet = self.browse(cr, uid, oid, context)          
+
+        timesheet = self.browse(cr, uid, oid, context)
         attendance_obj = self.pool.get("hr.attendance")
         attendance_ids = attendance_obj.search(cr,uid,[("sheet_id","=",timesheet.id)],order="name asc")
-        
+
         next_range = {
             "day" : None,
             "from" : None,
-            "to" : None            
+            "to" : None
         }
-                         
-        days = {}        
+
+        days = {}
         for att in attendance_obj.browse(cr,uid,attendance_ids,context):
-            cur_day = att.day            
-            if att.action == "sign_in":                    
+            cur_day = util.timeToDateStr(att.name)
+            if att.action == "sign_in":
                 next_range = {
                  "day" : cur_day,
                  "from" : helper.strToLocalTimeStr(cr,uid,att.name,context),
-                 "to" : None              
-                }                                                     
+                 "to" : None
+                }
                 _get_attendances(days,cur_day).append(next_range)
             elif att.action=="sign_out":
                 if next_range["day"] != cur_day:
@@ -170,31 +170,31 @@ class hr_timesheet(osv.osv):
                       "day" : cur_day,
                       "from" : None,
                       "to" : helper.strToLocalTimeStr(cr,uid,att.name,context)
-                    }                
+                    }
                     _get_attendances(days,cur_day).append(next_range)
                 else:
                     next_range["to"] = helper.strToLocalTimeStr(cr,uid,att.name,context)
-                    
-                    
+
+
         leaves = self.get_leaves(cr, uid, oid, context)
         date_from = util.strToDate(timesheet.date_from)
         date_to = util.strToDate(timesheet.date_to)
         date_cur = date_from
         delta_day = relativedelta(days=1)
         range_open = False
-        
+
         while date_cur <= date_to:
-            date_cur_str = util.dateToStr(date_cur)     
+            date_cur_str = util.dateToStr(date_cur)
             attendances = _get_attendances(days,date_cur_str)
-            
+
             days[date_cur_str]['total_attendance_day']=0.0
             days[date_cur_str]['total_timesheet_day']=0.0
             days[date_cur_str]['total_difference_day']=0.0
-            
+
             leaves_for_day = leaves.get(date_cur_str)
             if leaves_for_day:
                 days[date_cur_str]["leaves"]=leaves_for_day
-                
+
             if not attendances and range_open:
                 attendances.append( {
                     "day" : date_cur_str,
@@ -204,13 +204,13 @@ class hr_timesheet(osv.osv):
             elif attendances:
                 if range_open:
                     attendances[0]["from"] = date_cur_str + " 00:00:00"
-                    range_open = False                        
+                    range_open = False
                 last_range = attendances[-1]
                 if not last_range.get("to"):
-                    range_open = True                    
-                    last_range["to"]= util.timeToStr(date_cur+delta_day)                    
-            date_cur += delta_day   
-        
+                    range_open = True
+                    last_range["to"]= util.timeToStr(date_cur+delta_day)
+            date_cur += delta_day
+
         #get total days
         cr.execute('SELECT day.name, day.total_attendance, day.total_timesheet, day.total_difference\
                 FROM hr_timesheet_sheet_sheet AS sheet \
@@ -218,37 +218,37 @@ class hr_timesheet(osv.osv):
                     ON (sheet.id = day.sheet_id \
                         AND day.name IN %s) \
                 WHERE sheet.id = %s',(tuple(days.keys()),oid) )
-        
-        for total_day in cr.fetchall():       
-            if total_day[0]:     
+
+        for total_day in cr.fetchall():
+            if total_day[0]:
                 days[total_day[0]]['total_attendance_day'] = total_day[1]
                 days[total_day[0]]['total_timesheet_day'] = total_day[2]
                 days[total_day[0]]['total_difference_day'] = total_day[3]
-                
+
         #get analytic lines
-        line_obj = self.pool.get("hr.analytic.timesheet")        
+        line_obj = self.pool.get("hr.analytic.timesheet")
         for key,value in days.items():
-            value["lines"] = line_obj.search(cr, uid, [("date","=",key),("sheet_id","=",timesheet.id)], order="id asc")            
-                    
+            value["lines"] = line_obj.search(cr, uid, [("date","=",key),("sheet_id","=",timesheet.id)], order="id asc")
+
         return days
-    
+
     _inherit = "hr_timesheet_sheet.sheet"
 
 
 class hr_timesheet_line(osv.osv):
-    def _amount_brutto(self, cr, uid, ids, field_name, arg, context=None):      
+    def _amount_brutto(self, cr, uid, ids, field_name, arg, context=None):
         tax_obj= self.pool.get("account.tax")
-        res = {}        
+        res = {}
         for line in self.browse(cr, uid, ids, context=context):
             amount = 0.0
             product = line.product_id
-            if product:                
+            if product:
                 amount_all = tax_obj.compute_all(cr,uid,product.supplier_taxes_id,product.standard_price,1,product=product.id)
-                amount =  amount_all["total_included"]            
-            res[line.id]=amount            
-        return res 
-    
+                amount =  amount_all["total_included"]
+            res[line.id]=amount
+        return res
+
     _inherit = "hr.analytic.timesheet"
-    _columns = {            
+    _columns = {
         "amount_brutto" : fields.function(_amount_brutto,string="Amount (Brutto)",type="float", digits_compute=dp.get_precision("Account")),
     }
