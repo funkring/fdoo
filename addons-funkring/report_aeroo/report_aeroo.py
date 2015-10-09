033ff35c5acb838fa6f9b317b9c1ff37c8e88814
Martin Reisenhofer <martin.reisenhofer@funkring.net>
2015-10-09 11:37:05 +0200

    [FIX] account partner search

--- addons-funkring/report_aeroo/report_aeroo.py
+++ addons-funkring/report_aeroo/report_aeroo.py
@@ -634,33 +634,18 @@ class Aeroo_report(report_sxw):
                 
             if results:
                 if results[0][1]=='pdf':
-                    try:
-                        output = PdfFileWriter()
-                        total_pages = 0
-                        for r in results:
-                            reader = fixPdf(r[0],ret_reader=True)
-                            if reader:
-                                pages = 0
-                                for page in range(reader.getNumPages()):
-                                    pages += 1
-                                    output.addPage(reader.getPage(page))
-                                    
-                                if not pages:
-                                    logger.error("No pages to merge corrupted PDF %s - %s" % (r[2],r[3]))
-                                    
-                                tota_pages += pages
-                            else:
-                                logger.error("Unable to merge corrupted PDF %s - %s" % (r[2],r[3]))
-                        
-                        # only return if pages exist
-                        if total_pages:     
-                            s = StringIO()
-                            output.write(s)
-                            return s.getvalue(), results[0][1]
-                        
-                    except Exception as e:
-                        logger.error("Unable to merge corrupted PDF",e)
-                    
+                    output = PdfFileWriter()
+                    for r in results:
+                        reader = fixPdf(r[0],ret_reader=True)
+                        if reader: 
+                            for page in range(reader.getNumPages()):
+                                output.addPage(reader.getPage(page))
+                        else:
+                            logger.error("Unable to merge corrupted PDF %s - %s" % (r[2],r[3]))
+                                
+                    s = StringIO()
+                    output.write(s)
+                    return s.getvalue(), results[0][1]
         return self.create_single_pdf(cr, uid, ids, data, report_xml, context)
 
     def create_source_odt(self, cr, uid, ids, data, report_xml, context=None):
