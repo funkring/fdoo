50ae293c182f851cca13362edba28991573bc477
Martin Reisenhofer <martin.reisenhofer@funkring.net>
2017-07-04 14:26:21 +0200

    [MOVED] academy to funkring extras

--- addons-funkring/academy/__init__.py
+++
@@ -1,6 +0,0 @@
-import academy
-import company
-import res_config
-import wizard
-import product
-import account_invoice
--- addons-funkring/academy/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-import invoice_assistant
-import semester_assistant
-import post_payment_wizard
-import assign_registration_wizard
-import trainer_reg_wizard
